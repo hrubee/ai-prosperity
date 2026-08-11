885f1ddd0ea4c5e193a720807682346f88823ee7
hrubee <85213582+hrubee@users.noreply.github.com>
2026-08-11 09:53:54 +0530

    Fix WebSocket ConnectionManager disconnect safety

--- backend/app/copier.py
+++ backend/app/copier.py
@@ -52,10 +52,12 @@ class ConnectionManager:
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        if websocket not in self.active_connections:
+            self.active_connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        if websocket in self.active_connections:
+            self.active_connections.remove(websocket)
 
     async def broadcast(self, message: str):
         save_log(message)
